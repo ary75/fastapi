0a67b0ee517f9241df5ee081bdfd9e956940e853
ary75 <156979961+ary75@users.noreply.github.com>
2024-05-13 17:52:26 +0800

    message

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
